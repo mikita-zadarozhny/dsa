fea498e6172a2c91703f09ce6a50f3f986e606d8
mikita-zadarozhny <nikita.zadorozhny@icloud.com>
2026-06-15 04:08:20 +0400

    FenwickTree (a.k.a Binary Indexed Tree) data structure was added.

---
+++ src/main/java/org/mikita/datastructure/tree/FenwickTree.java
@@ -0,0 +1,46 @@
+package org.mikita.datastructure.tree;
+
+import java.util.Arrays;
+
+/**
+ * Also known as Binary Indexed Tree (BIT)
+ */
+public class FenwickTree {
+
+    private final int size;
+    private final int[] nodes;
+
+    public FenwickTree(int[] data) {
+        size = data.length;
+        nodes = new int[size + 1];
+        Arrays.fill(nodes, Integer.MIN_VALUE);
+
+        for(int i = 0; i < size; i++) {
+            update(i, data[i]);
+        }
+    }
+
+    public void update(int index, int delta) {
+        index = index + 1; // 0-indexed -> 1-indexed
+
+        while (index <= size) {
+            nodes[index] += delta;
+            // to move to ancestor, use the following formula:
+            // ancestor(index) = index + LSB (Least Significant Bit)
+            index += index & (-index);
+        }
+    }
+
+    public int query(int index) {
+        index = index + 1; // 0-indexed -> 1-indexed
+
+        int result = 0;
+        while (index > 0) {
+            result += nodes[index];
+            // to move to parent, use the following formula:
+            // parent(index) = index - LSB (Least Significant Bit)
+            index -= index & (-index);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/org/mikita/datastructure/tree/FenwickTreeTest.java
@@ -0,0 +1,56 @@
+package org.mikita.datastructure.tree;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FenwickTreeTest {
+
+    public static Stream<Arguments> queryTestCases() {
+        return Stream.of(
+                Arguments.of(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, 5, 21),
+                Arguments.of(new int[] {1, 2, 3, 4, 1, 6, 7, 8, 9}, 5, 17),
+                Arguments.of(new int[] {1, 2, 3, 4, 10, 6, 7, 8, 9}, 5, 26),
+                Arguments.of(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, 8, 45)
+        );
+    }
+
+    public static Stream<Arguments> queryAndUpdateTestCases() {
+        return Stream.of(
+                Arguments.of(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, 4, 1, 5, 22),
+                Arguments.of(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, 4, 10, 5, 31)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("queryTestCases")
+    void shouldQuery(int[] data, int index, int expectedResult) {
+        // given
+        FenwickTree fenwickTree = new FenwickTree(data);
+
+        // when
+        int actual = fenwickTree.query(index);
+
+        // then
+        assertEquals(expectedResult, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("queryAndUpdateTestCases")
+    void shouldUpdate_thenQuery(int[] data, int updateIndex, int updateValue,
+                                int index, int expectedResult) {
+        // given
+        FenwickTree fenwickTree = new FenwickTree(data);
+
+        // when
+        fenwickTree.update(updateIndex, updateValue);
+        int actual = fenwickTree.query(index);
+
+        // then
+        assertEquals(expectedResult, actual);
+    }
+}
