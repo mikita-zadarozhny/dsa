75e5b3ed6193d50d12885ad62619187654326146
mikita-zadarozhny <nikita.zadorozhny@icloud.com>
2026-07-11 03:54:30 +0400

    Binary Lifting code was cleaned up.

--- src/main/java/org/mikita/algorithm/graph/BinaryLifting.java
+++ src/main/java/org/mikita/algorithm/graph/BinaryLifting.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 
 public class BinaryLifting {
 
-    // lookupTable[i][j] -> 2^jth ancestor of i;
+    /**
+     * lookupTable[i][j] -> 2^jth ancestor of i;
+     */
     private final int[][] lookupTable;
     private final int treeSize;
     private final int treeSizeLog;
@@ -17,9 +19,11 @@ public class BinaryLifting {
         precompute(parent);
     }
 
-    // rounded down
+    /**
+     * rounded down
+     */
     protected int log2(int num) {
-        return (int)(Math.log10(num)/Math.log10(2));
+        return (int) (Math.log10(num) / Math.log10(2));
     }
 
     /**
@@ -28,30 +32,26 @@ public class BinaryLifting {
      */
     private void precompute(int[] parent) {
 
-        for(int i = 0; i < treeSize; i++) {
+        for (int i = 0; i < treeSize; i++) {
             Arrays.fill(lookupTable[i], -1);
             lookupTable[i][0] = parent[i];
         }
 
-        for(int j = 1; j < treeSizeLog; j++) {
-            for(int node = 0; node < treeSize; node++) {
-                if(lookupTable[node][j - 1] != -1) {
+        for (int j = 1; j < treeSizeLog; j++) {
+            for (int node = 0; node < treeSize; node++) {
+                if (lookupTable[node][j - 1] != -1) {
                     lookupTable[node][j] = lookupTable[lookupTable[node][j - 1]][j - 1];
                 }
             }
         }
     }
 
-    // 19 = 16 + 2 + 1
-    // log2(19) = 4 => 19 - 16 = 3
-    // log2(3) = 1 => 3 - 2 = 1
-    // log2(1) = 0 => 1 - 1 = 0
     public int getKthAncestor(int nodeIndex, int k) {
-        for(int j = 0; j < treeSizeLog; j++) {
-            if((k & (1 << j)) != 0) {
+        for (int j = 0; j < treeSizeLog; j++) {
+            if ((k & (1 << j)) != 0) {
                 nodeIndex = lookupTable[nodeIndex][j];
             }
-            if(nodeIndex == -1) {
+            if (nodeIndex == -1) {
                 return -1;
             }
         }
@@ -61,7 +61,7 @@ public class BinaryLifting {
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
-        for(int[] row : lookupTable) {
+        for (int[] row : lookupTable) {
             result.append(Arrays.toString(row)).append(System.lineSeparator());
         }
         return result.toString();
--- src/test/java/org/mikita/algorithm/graph/BinaryLiftingTest.java
+++ src/test/java/org/mikita/algorithm/graph/BinaryLiftingTest.java
@@ -19,16 +19,16 @@ class BinaryLiftingTest {
                 Arguments.of(new int[]{-1, 2, 3, 0}, 2, 1, 3),
                 Arguments.of(new int[]{-1, 2, 3, 0}, 2, 3, -1),
                 Arguments.of(new int[]{-1, 2, 3, 0}, 2, 2, 0),
-                Arguments.of(new int[]{-1,0,0,1,2,0,1,3,6,1}, 8, 6, -1),
-                Arguments.of(new int[]{-1,0,0,1,2,0,1,3,6,1}, 9, 7, -1),
-                Arguments.of(new int[]{-1,0,0,1,2,0,1,3,6,1}, 1, 1, 0),
-                Arguments.of(new int[]{-1,0,0,1,2,0,1,3,6,1}, 2, 5, -1),
-                Arguments.of(new int[]{-1,0,0,1,2,0,1,3,6,1}, 4, 2, 0),
-                Arguments.of(new int[]{-1,0,0,1,2,0,1,3,6,1}, 7, 3, 0),
-                Arguments.of(new int[]{-1,0,0,1,2,0,1,3,6,1}, 3, 7, -1),
-                Arguments.of(new int[]{-1,0,0,1,2,0,1,3,6,1}, 9, 6, -1),
-                Arguments.of(new int[]{-1,0,0,1,2,0,1,3,6,1}, 3, 5, -1),
-                Arguments.of(new int[]{-1,0,0,1,2,0,1,3,6,1}, 8, 8, -1)
+                Arguments.of(new int[]{-1, 0, 0, 1, 2, 0, 1, 3, 6, 1}, 8, 6, -1),
+                Arguments.of(new int[]{-1, 0, 0, 1, 2, 0, 1, 3, 6, 1}, 9, 7, -1),
+                Arguments.of(new int[]{-1, 0, 0, 1, 2, 0, 1, 3, 6, 1}, 1, 1, 0),
+                Arguments.of(new int[]{-1, 0, 0, 1, 2, 0, 1, 3, 6, 1}, 2, 5, -1),
+                Arguments.of(new int[]{-1, 0, 0, 1, 2, 0, 1, 3, 6, 1}, 4, 2, 0),
+                Arguments.of(new int[]{-1, 0, 0, 1, 2, 0, 1, 3, 6, 1}, 7, 3, 0),
+                Arguments.of(new int[]{-1, 0, 0, 1, 2, 0, 1, 3, 6, 1}, 3, 7, -1),
+                Arguments.of(new int[]{-1, 0, 0, 1, 2, 0, 1, 3, 6, 1}, 9, 6, -1),
+                Arguments.of(new int[]{-1, 0, 0, 1, 2, 0, 1, 3, 6, 1}, 3, 5, -1),
+                Arguments.of(new int[]{-1, 0, 0, 1, 2, 0, 1, 3, 6, 1}, 8, 8, -1)
         );
     }
 
