621c7563b7f0e3baf26d762a825e764ca72e908b
mikita-zadarozhny <nikita.zadorozhny@icloud.com>
2026-07-23 03:25:03 +0400

    Testing approach for PrimTest and KruskalTest was refactored to enhance code (test cases) reusability.

--- src/main/java/org/mikita/algorithm/graph/mst/prim/Prim.java
+++ src/main/java/org/mikita/algorithm/graph/mst/prim/Prim.java
@@ -10,11 +10,16 @@ import java.util.List;
 public class Prim {
 
     public int[][] findMinimalSpanningTree(int nodes, List<List<int[]>> adjacencyList) {
+        return findMinimalSpanningTree(nodes, 0, adjacencyList);
+    }
+
+    public int[][] findMinimalSpanningTree(int nodes, int startNode, List<List<int[]>> adjacencyList) {
+        int leftToVisit = nodes;
         boolean[] visited = new boolean[nodes];
         Heap<int[]> heap = ArrayHeap.minHeap(Comparator.comparingInt(o -> o[2]));
 
         // current node, prev node, edge cost
-        heap.add(new int[]{0, -1, 0});
+        heap.add(new int[]{startNode, -1, 0});
 
         List<int[]> result = new ArrayList<>();
         while(!heap.isEmpty()) {
@@ -27,16 +32,26 @@ public class Prim {
             }
 
             visited[nodeNumber] = true;
+            leftToVisit--;
+
             if(prevNode != -1) {
                 result.add(new int[] {nodeNumber, prevNode, edgeCost});
             }
 
-            for(int[] adjacentNode : adjacencyList.get(node[0])) {
+            if(adjacencyList.size() <= nodeNumber) {
+                continue;
+            }
+
+            for(int[] adjacentNode : adjacencyList.get(nodeNumber)) {
                 if(visited[adjacentNode[0]]) {
                     continue;
                 }
                 heap.add(new int[] {adjacentNode[0], nodeNumber, adjacentNode[1]});
             }
+        }
+
+        if(leftToVisit != 0) {
+            throw new IllegalArgumentException("Prim's algorithm is not compatible with disjoint graphs");
         }
 
         return result.toArray(new int[0][]);
--- src/test/java/org/mikita/algorithm/graph/mst/prim/PrimTest.java
+++ src/test/java/org/mikita/algorithm/graph/mst/prim/PrimTest.java
@@ -1,166 +1,37 @@
 package org.mikita.algorithm.graph.mst.prim;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Stream;
+import org.mikita.algorithm.graph.mst.BaseMinimumSpanningTreeTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PrimTest {
+class PrimTest extends BaseMinimumSpanningTreeTest {
 
-    public static Stream<Arguments> mstTestCases() {
-        return Stream.of(
-                // 1. Basic triangle
-                Arguments.of(
-                        3,
-                        List.of(
-                                List.of(new int[]{1, 1}, new int[]{2, 3}), // 0
-                                List.of(new int[]{0, 1}, new int[]{2, 2}), // 1
-                                List.of(new int[]{1, 2}, new int[]{0, 3})  // 2
-                        ),
-                        new int[][]{
-                                {0, 1, 1},
-                                {1, 2, 2}
-                        }
-                ),
-
-                // 2. Unsorted edges
-                Arguments.of(
-                        4,
-                        List.of(
-                                List.of(new int[]{1, 10}, new int[]{2, 6}, new int[]{3, 5}), // 0
-                                List.of(new int[]{0, 10}, new int[]{3, 15}),                // 1
-                                List.of(new int[]{0, 6}, new int[]{3, 4}),                 // 2
-                                List.of(new int[]{0, 5}, new int[]{1, 15}, new int[]{2, 4}) // 3
-                        ),
-                        new int[][]{
-                                {2, 3, 4},
-                                {0, 3, 5},
-                                {0, 1, 10}
-                        }
-                ),
-
-                // 3. Multiple valid MSTs
-                Arguments.of(
-                        4,
-                        List.of(
-                                List.of(new int[]{1, 1}, new int[]{3, 1}), // 0
-                                List.of(new int[]{0, 1}, new int[]{2, 1}), // 1
-                                List.of(new int[]{1, 1}, new int[]{3, 1}), // 2
-                                List.of(new int[]{2, 1}, new int[]{0, 1})  // 3
-                        ),
-                        new int[][]{
-                                {0, 1, 1},
-                                {1, 2, 1},
-                                {2, 3, 1}
-                        }
-                ),
-
-                // 4. Disconnected graph -> should fail
-//                Arguments.of(
-//                        5,
-//                        List.of(
-//                                List.of(new int[]{1, 1}), // 0
-//                                List.of(new int[]{0, 1}), // 1
-//                                List.of(new int[]{3, 2}), // 2
-//                                List.of(new int[]{2, 2}), // 3
-//                                List.of()                 // 4
-//                        ),
-//                        new int[][]{
-//                                {0, 1, 1},
-//                                {2, 3, 2}
-//                        }
-//                ),
-
-                // 5. Single node
-                Arguments.of(
-                        1,
-                        List.of(
-                                List.of()
-                        ),
-                        new int[][]{}
-                ),
-
-                // 6. Two nodes
-                Arguments.of(
-                        2,
-                        List.of(
-                                List.of(new int[]{1, 5}), // 0
-                                List.of(new int[]{0, 5})  // 1
-                        ),
-                        new int[][]{
-                                {0, 1, 5}
-                        }
-                ),
-
-                // 7. Cycle-heavy graph
-                Arguments.of(
-                        4,
-                        List.of(
-                                List.of(new int[]{1, 1}, new int[]{3, 4}, new int[]{2, 5}), // 0
-                                List.of(new int[]{0, 1}, new int[]{2, 2}),                // 1
-                                List.of(new int[]{1, 2}, new int[]{3, 3}, new int[]{0, 5}), // 2
-                                List.of(new int[]{2, 3}, new int[]{0, 4})                 // 3
-                        ),
-                        new int[][]{
-                                {0, 1, 1},
-                                {1, 2, 2},
-                                {2, 3, 3}
-                        }
-                ),
-
-                // 8. Negative weights
-                Arguments.of(
-                        3,
-                        List.of(
-                                List.of(new int[]{1, -2}, new int[]{2, 4}), // 0
-                                List.of(new int[]{0, -2}, new int[]{2, -1}), // 1
-                                List.of(new int[]{1, -1}, new int[]{0, 4})  // 2
-                        ),
-                        new int[][]{
-                                {0, 1, -2},
-                                {1, 2, -1}
-                        }
-                ),
-
-                // 9. Already optimal
-                Arguments.of(
-                        4,
-                        List.of(
-                                List.of(new int[]{1, 1}), // 0
-                                List.of(new int[]{0, 1}, new int[]{2, 2}), // 1
-                                List.of(new int[]{1, 2}, new int[]{3, 3}), // 2
-                                List.of(new int[]{2, 3})  // 3
-                        ),
-                        new int[][]{
-                                {0, 1, 1},
-                                {1, 2, 2},
-                                {2, 3, 3}
-                        }
-                )
-        );
-    }
-
-    @MethodSource("mstTestCases")
+    @MethodSource("connectedGraphs")
     @ParameterizedTest
-    void findMinimalSpanningTree(int nodes, List<List<int[]>> adjacentNodes, int[][] expected) {
+    void shouldFindMinimalSpanningTree_whenGraphIsConnected(int nodes, int[][] edges, int[][] expected) {
         // given
         Prim prim = new Prim();
 
         // when
-        int[][] actual = prim.findMinimalSpanningTree(nodes, adjacentNodes);
+        int[][] actual = prim.findMinimalSpanningTree(nodes, edgesToAdjacencyList(edges));
 
-        //then
+        // then
         assertEquals(totalWeight(expected), totalWeight(actual));
     }
 
-    private int totalWeight(int[][] edges) {
-        return Arrays.stream(edges)
-                .mapToInt(e -> e[2])
-                .sum();
+    @MethodSource("disjointGraphs")
+    @ParameterizedTest
+    void shouldThrowException_whenGraphIsDisjoint(int nodes, int[][] edges) {
+        // given
+        Prim prim = new Prim();
+
+        // when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> prim.findMinimalSpanningTree(nodes, edgesToAdjacencyList(edges)));
+
+        // then
+        assertEquals("Prim's algorithm is not compatible with disjoint graphs", exception.getMessage());
     }
 }
