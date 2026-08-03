9d2cba04a3aa7262c7d98603a724ece0f9927f47
mikita-zadarozhny <nikita.zadorozhny@icloud.com>
2026-08-03 17:58:14 +0400

    Division in FixedSizeArrayBasedBitArray was refactored to use bit operations.

--- src/main/java/org/mikita/datastructure/bit/bitarray/impl/FixedSizeArrayBasedBitArray.java
+++ src/main/java/org/mikita/datastructure/bit/bitarray/impl/FixedSizeArrayBasedBitArray.java
@@ -5,56 +5,74 @@ import org.mikita.datastructure.bit.bitarray.BitArray;
 public class FixedSizeArrayBasedBitArray implements BitArray {
 
     private final int size;
-    private final long[] bits;
-    private final int arraySize;
+    private final long[] words;
 
     public FixedSizeArrayBasedBitArray(int size) {
+        if (size < 0) {
+            throw new IllegalArgumentException("Size cannot be negative");
+        }
+
         this.size = size;
-        this.arraySize = (int)Math.ceil((double)size / Long.SIZE);
-        this.bits = new long[arraySize];
+        int numberOfWords = (int)Math.ceil((double)size / Long.SIZE);
+        this.words = new long[numberOfWords];
     }
 
     @Override
     public void set(int index) {
         validate(index);
 
-        int targetArray = index / Long.SIZE;
-        int targetBit = index % Long.SIZE;
-        bits[targetArray] = bits[targetArray] | (1L << targetBit);
+        // division by 64
+        int targetWord = index >>> 6;
+
+        // index modulo 64
+        int targetBit = index & 63;
+
+        words[targetWord] = words[targetWord] | (1L << targetBit);
     }
 
     @Override
     public boolean flip(int index) {
         validate(index);
 
-        int targetArray = index / Long.SIZE;
-        int targetBit = index % Long.SIZE;
-        bits[targetArray] = bits[targetArray] ^ (1L << targetBit);
+        // division by 64
+        int targetWord = index >>> 6;
 
-        return (bits[targetArray] & (1L << targetBit)) != 0;
+        // index modulo 64
+        int targetBit = index & 63;
+
+        words[targetWord] = words[targetWord] ^ (1L << targetBit);
+
+        return (words[targetWord] & (1L << targetBit)) != 0;
     }
 
     @Override
     public void clear(int index) {
         validate(index);
 
-        int targetArray = index / Long.SIZE;
-        int targetBit = index % Long.SIZE;
-        bits[targetArray] = bits[targetArray] & (~(1L << targetBit));
+        // division by 64
+        int targetWord = index >>> 6;
+
+        // index modulo 64
+        int targetBit = index & 63;
+
+        words[targetWord] = words[targetWord] & (~(1L << targetBit));
     }
 
     @Override
     public boolean get(int index) {
         validate(index);
 
-        int targetArray = index / Long.SIZE;
-        int targetBit = index % Long.SIZE;
+        // division by 64
+        int targetWord = index >>> 6;
 
-        return (bits[targetArray] & (1L << targetBit)) != 0;
+        // index modulo 64
+        int targetBit = index & 63;
+
+        return (words[targetWord] & (1L << targetBit)) != 0;
     }
 
     private void validate(int index) {
-        if(index >= size) {
+        if(index < 0 || index >= size) {
             throw new IndexOutOfBoundsException(index);
         }
     }
